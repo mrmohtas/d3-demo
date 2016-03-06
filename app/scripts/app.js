40c7da440454ebda8761294c4dcfd81bc8bf9496
Tom Sturge <tomsturge+github@gmail.com>
2016-03-06 17:11:26 +0000

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name d3DemoApp
+ * @description
+ * # d3DemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('d3DemoApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name d3DemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the d3DemoApp
+ */
+angular.module('d3DemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
